0c2ef9012b967b8de3adf19d340eef1453ea8350
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-11-18 11:05:43 -0800

    Updated documentation

--- request/getitem.php
+++ request/getitem.php
@@ -4,13 +4,20 @@
 	* @copyright 2014, Chris Zuber <chris@kernrivercorp.com>
 	* @package mother_brain
 	* @version 2014-11-18
-	* @link http://developer.ebay.com/devzone/xml/docs/reference/ebay/AddItem.html
+	* @link http://developer.ebay.com/devzone/xml/docs/reference/ebay/GetItem.html
 	*/
 
 	namespace eBay_API\Request;
 	use \core\resources\XML_Node as XML_Node;
 	use \core\_pdo as PDO;
 	class GetItem extends \eBay_API\eBay_API_Call {
+		/**
+		 * Construct a new eBay API request of the correct type
+		 *
+		 * @param string $store   [Store name to get credentials for]
+		 * @param bool   $sandbox [Production or sandbox environment]
+		 */
+
 		public function __construct($store, $sandbox = false) {
 			parent::__construct($store, 'GetItem', $sandbox);
 
