726b679f10b3fbc19cf9a787d3637cf895e5648f
Chris Zuber <shgysk8zer0@gmail.com>
2014-12-03 13:34:03 -0800

    Updated documentation

--- ebay_api_call.class.php
+++ ebay_api_call.class.php
@@ -27,15 +27,8 @@
 		 * Dynamically construct an XML_API_Call with eBay specific paramaters
 		 *
 		 * @param string  $store    [User in database, used to get credentials]
-		 * @param string  $callname [Which type of request?]
 		 * @param bool    $sandbox  [Sandbox or production]
-		 * @param integer $level    [X-EBAY-API-COMPATIBILITY-LEVEL]
-		 * @param integer $siteID   [X-EBAY-API-SITEID]
-		 * @param string  $urn      [Namespace to use]
 		 * @param bool    $verbose  [Use CURLOPT_VERBOSE?]
-		 * @param string  $charset  [Charset for XML]
-		 * @param string  $type     [enctype of request body]
-		 * @param string  $boundary [Designates a boundry in request body]
 		 *
 		 * @var string $environment [Based on $sandbox, will be either 'production' or 'sandbox']
 		 * @var string $url [URL to send request to, based on $sandbox]
@@ -67,7 +60,6 @@
 		 * Create a new XML_API call with eBay specific data
 		 *
 		 * @param string $store    [Store name making request]
-		 * @param string $callname [Callname for request]
 		 * @param bool   $sandbox  [Production or sandbox environment]
 		 * @param bool   $verbose  [Use verbose in cURL request]
 		 */
